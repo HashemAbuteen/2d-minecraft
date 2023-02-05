bc295f2d54d3a404852fb2af863a91e803f3a842
Hashem <hashemabualteen@gmail.com>
2023-02-05 02:36:21 +0200

    generating trees

--- script.js
+++ script.js
@@ -1,47 +1,102 @@
 const gameContainer = document.getElementsByClassName("game-container")[0];
 
 //generate the world
-for (let i = 30; i > 0; i--) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.y = i;
-    row.id = "row-" + i;
-    gameContainer.appendChild(row);
-    for (let j = 0; j < 100; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.x = j;
-        cell.y = i;
-        cell.id = "cell-" + j +"-" + i;
-        //grass blocks
-        if(i === 20){
-            cell.setAttribute("blocktype" , "grass");
+function generateTheWorld(){
+    for (let i = 30; i > 0; i--) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.y = i;
+        row.id = "row-" + i;
+        gameContainer.appendChild(row);
+        for (let j = 0; j < 100; j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.x = j;
+            cell.y = i;
+            cell.id = "cell-" + j +"-" + i;
+            //grass blocks
+            if(i === 20){
+                cell.setAttribute("blocktype" , "grass");
+            }
+            //dirt blocks
+            if(i<20 && i >15){
+                cell.setAttribute("blocktype" , "dirt");
+            }
+            //stone blocks
+            if(i<=15 && i>2){
+                cell.setAttribute("blocktype" , "stone");
+            }
+            if(i<=2 && i>0){
+                cell.setAttribute("blocktype" , "bedrock");
+            }
+            row.appendChild(cell);
         }
-        //dirt blocks
-        if(i<20 && i >15){
-            cell.setAttribute("blocktype" , "dirt");
-        }
-        //stone blocks
-        if(i<=15 && i>2){
-            cell.setAttribute("blocktype" , "stone");
-        }
-        if(i<=2 && i>0){
-            cell.setAttribute("blocktype" , "bedrock");
-        }
-        row.appendChild(cell);
     }
 }
+generateTheWorld();
+
+//generate trees
+function generateTrees(){
+    const grassCells = document.querySelectorAll("[blocktype='grass']");
+    const numOfTrees = Math.ceil(Math.random()*grassCells.length/4);
+    console.log(numOfTrees);
+    for(let i =0 ; i < numOfTrees ; i++){
+        const randomGrassCell = grassCells[Math.floor(Math.random()*grassCells.length)];
+        if(randomGrassCell.getAttribute("hasATree")){
+            console.log("conflict");
+            continue;
+        }
+        else{
+            setTreeRestrictions(randomGrassCell);
+            generateOakTree(randomGrassCell);
+        }
+    }
+}
+
+function generateOakTree(ground){
+    console.log("generating atree");
+    const height = Math.floor(Math.random()*3)+6;
+    const id = ground.id;
+    const x = id.split('-')[1];
+    let y = parseInt(id.split('-')[2]);
+    for(let i = 0; i<height ; i++){
+        y++;
+        const logCell = document.getElementById("cell-"+x +"-" + y);
+        if(logCell && !logCell.getAttribute("blocktype")){
+            logCell.setAttribute("blocktype" , "oak-log");
+            setBackground(logCell);
+        }
+    }
+}
+
+//this is a function to not get trees to generate next to each other
+function setTreeRestrictions(ground){
+    ground.setAttribute("hasATree" , true);
+    const id = ground.id;
+    let x = parseInt(id.split('-')[1]);
+    const y = parseInt(id.split('-')[2]);
+    x = x-3;
+    for(let i =0; i<7 ;i++){
+        const cell = document.getElementById("cell-" + x +"-" + y);
+        if(cell){
+            cell.setAttribute("hasATree" , true);
+        }
+        x++;
+    } 
+}
+generateTrees();
 
 //constants define what is a block and what is a tool and the type of the tool
 const shovels = ["diamond-shovel"];
 const axes = ["diamond-axe"];
 const pickaxes = ["diamond-pickaxe"];
-const blocks = ["dirt" , "stone" , "grass"];
+const blocks = ["dirt" , "stone" , "grass" ,"oak-log"];
 //what tool break each block
 const blockTool = {
     dirt : shovels,
     stone : pickaxes,
     grass : shovels,
+    "oak-log" : axes,
 }
 
 //hand item is the tool/block chosen from inventory
